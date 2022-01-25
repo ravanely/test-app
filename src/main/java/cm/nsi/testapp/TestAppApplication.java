939e13c88c62b18c60f7569753437cb6e6f4ba0b
Fabrice Nague <nagueravanely@gmail.com>
2022-01-25 16:45:44 +0100

    Update TestAppApplication.java

--- src/main/java/cm/nsi/testapp/TestAppApplication.java
+++ src/main/java/cm/nsi/testapp/TestAppApplication.java
@@ -8,6 +8,8 @@ public class TestAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestAppApplication.class, args);
+        
+        System.out.println("Hello World !");
     }
 
 }
